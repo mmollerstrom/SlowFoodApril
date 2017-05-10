a9b52173afd56834917aff68d105ac124b196cc7
mmollerstrom <mmollerstrom@icloud.com>
2017-05-10 16:15:56 +0200

    -am

---
+++ features/step_definitions/index_steps.rb
@@ -0,0 +1,11 @@
+When(/^I visit the landing page$/) do
+  visit root_path
+end
+
+Then(/^I should see Restaurant "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should see "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
