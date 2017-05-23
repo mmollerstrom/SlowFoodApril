87ea19320ba3302764cfa0dd91d496fdb7cb7445
claes <claesca@outlook.com>
2017-05-23 15:23:50 +0200

    add menu name on restaurant menu page

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,3 +1,9 @@
 class MenusController < ApplicationController
-
+  def show
+    # restaurant = Restaurant.find(params[:restaurant_id])
+    # @menu = restaurant.menus.find(params[:id])
+    # Menu.find(params[:id])
+    @menu = Menu.where("restaurant_id = ?", params[:restaurant_id].to_i).first
+    binding.pry
+  end
 end
