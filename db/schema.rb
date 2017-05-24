37ddcc3e71c54f25610691fe5e0a2884b40d6232
junydania <o.dania@aol.com>
2017-05-24 17:22:36 +0200

    created a migration file to add city, state and country

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170514145414) do
+ActiveRecord::Schema.define(version: 20170524151657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,9 @@ ActiveRecord::Schema.define(version: 20170514145414) do
     t.datetime "updated_at",       null: false
     t.string   "description"
     t.integer  "rest_category_id"
+    t.string   "city"
+    t.string   "state"
+    t.string   "country"
     t.index ["rest_category_id"], name: "index_restaurants_on_rest_category_id", using: :btree
   end
 
