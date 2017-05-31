530dcc1be9a8c30b7147700338f951cbd2fc99e2
mmollerstrom <mmollerstrom@icloud.com>
2017-05-31 12:49:05 -0400

    initiate test for add dish to menu WIP

--- app/controllers/owners/restaurants_controller.rb
+++ app/controllers/owners/restaurants_controller.rb
@@ -5,5 +5,7 @@ class Owners::RestaurantsController < ApplicationController
 
   def show
     @restaurant_id = params[:id]
+    new_menu = Menu.create(name: "Orchid Bistro", restaurant_id: @restaurant_id)
+    @menu = Menu.all
   end
 end
