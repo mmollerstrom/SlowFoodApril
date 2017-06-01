725a6004d999ac39fdbccfd216e74eb8c9d06641
junydania <o.dania@aol.com>
2017-06-01 23:59:41 +0200

    test to fill the form creating new dish pass

--- app/controllers/owners/dishes_controller.rb
+++ app/controllers/owners/dishes_controller.rb
@@ -1,2 +1,9 @@
 class Owners::DishesController < ApplicationController
+
+  def new
+    @menu = Menu.find(params[:menu_id])
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @dish = Dish.new
+  end
+
 end
